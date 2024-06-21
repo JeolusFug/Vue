d35261039fd260bb0302c87cc152bdda0ed08d8e
JeolusFug <155438195+JeolusFug@users.noreply.github.com>
2024-06-21 14:07:10 +0900

    Add files via upload

---
+++ vue/first-vue/7_chanken/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+import chanken from './chanken.vue';
+
+new Vue(chanken).$mount('#root');  // 기존 el과 같은 역할
---
+++ vue/first-vue/8_lottoGen/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+import lottoGen from './lottoGen.vue';
+
+new Vue(lottoGen).$mount('#root');  // 기존 el과 같은 역할
